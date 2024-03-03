9594cc5909c6e1905d1bfb9617b0eb92e58c24a3
Владислав Коровин <skyline_blade@mail.ru>
2024-03-03 12:00:03 +0300

    создали общенный интерфейс репозитория IRepository c базовой функциональностью CRUD

--- StorePhones.DAL/Interfaces/IRepository.cs
+++ StorePhones.DAL/Interfaces/IRepository.cs
@@ -1,7 +1,12 @@
 ﻿
 namespace StorePhones.DAL.Interfaces
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
+        Task AddAsync(T entity);
+        Task<T> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
     }
 }
